32fa12ddfe5d9b536e964a60ef9678db68864b29
Robert Juzak <robert.juzak@twentyzen.com>
2018-03-19 16:24:46 +0100

    updated version 	updated translations

--- Config/config.php
+++ Config/config.php
@@ -12,6 +12,6 @@
 return [
     'name'        => 'Podio as CRM',
     'description' => 'Podio as CRM',
-    'version'     => '0.5.0',
+    'version'     => '0.5.1',
     'author'      => 'robjuz'
 ];
